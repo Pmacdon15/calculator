aac5f47e89bfefb9d04bfb7c2f50bc612cb75a8f
Patrick MacDonald <patrick@patmac.ca>
2025-07-12 21:10:12 -0600

    some css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
 
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-2 md:p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       <Calculator />
     </div>
   );
